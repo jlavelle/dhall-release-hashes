3f27ebe5dcc22de34eeff858e69f2a78f7705587
John C. Lavelle <jlavelle604@gmail.com>
2021-02-19 13:51:39 -0500

    Update hls

--- shell.nix
+++ shell.nix
@@ -1,18 +1,19 @@
 let sources = import ./nix/sources.nix;
     pkgs = import sources.nixpkgs {};
     github-utils = import ./default.nix;
+    hls = import sources.all-hls { inherit pkgs; version = "0.9.0"; ghc = "8.8.4"; };
 in
 github-utils.shellFor {
   withHoogle = false;
   tools = {
     cabal = "3.2.0.0";
-    haskell-language-server = "0.5.1";
     hlint = "3.2.1";
   };
   buildInputs = with pkgs.haskellPackages;
     [ ghcid
       hpack
       stylish-haskell
+      hls
     ];
   exactDeps = true;
 }
